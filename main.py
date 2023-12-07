1e36011cee23858e6d7f3cf5bf27347d300f6705
aman_mandal <30326171+mandalaman@users.noreply.github.com>
2023-12-07 21:11:04 +0530

    python simple projects

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-jk
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pygame
+from pygame.locals import *
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.init()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    surface = pygame.display.set_mode((1000, 500))
+    pygame.display.flip()
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                pass
+
+            elif event.type == QUIT:
+                running = False
+
+
