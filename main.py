5cbfafdcd592351ed60077c6c3524a415252c310
aman_mandal <30326171+mandalaman@users.noreply.github.com>
2023-12-08 21:24:23 +0530

    python simple projects

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pygame
 from pygame.locals import *
 
+def draw_block():
+    surface.fill((50, 168, 82))
+    surface.blit(block,(block_x, block_y))
+
+    pygame.display.flip()
+
 
 if __name__ == '__main__':
     pygame.init()
 
     surface = pygame.display.set_mode((1000, 500))
+    surface.fill((50, 168, 82))
+    block = pygame.image.load('resource/block.jpg').convert()
+    block_x = 100
+    block_y = 100
+    surface.blit(block,(block_x, block_y))
     pygame.display.flip()
 
     running = True
@@ -14,6 +25,18 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 pass
+                if event.key == K_UP:
+                    block_y = block_y - 10
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y = block_y + 10
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x = block_x - 10
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x = block_x + 10
+                    draw_block()
 
             elif event.type == QUIT:
                 running = False
